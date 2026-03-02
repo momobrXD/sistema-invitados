b4e45cd3e822b7b4554cef1b6d6facea9db3b976
Martin <marti45736gg@gmail.com>
2026-03-01 19:43:50 -0500

    Migracion a google-auth y optimizacion de dependencias1.1

--- app.py
+++ app.py
@@ -531,6 +531,6 @@ def agregar_invitado():
     else:
         flash(f"Invitado {nombre} agregado exitosamente")
         return redirect(url_for('index'))
-
+                
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
